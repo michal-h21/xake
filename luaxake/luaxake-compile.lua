cfdab2863405474f21aa4f8c177ebe4e0375f455
Michal Hoftich <michal.h21@gmail.com>
2024-09-18 17:07:40 +0200

    really compile

--- luaxake/luaxake-compile.lua
+++ luaxake/luaxake-compile.lua
@@ -18,6 +18,14 @@ end
 
 
 local function test_log_file(filename)
+  local f = io.open(filename, "r")
+  if not f then 
+    log:error("Cannot open log file: " .. filename)
+    return nil 
+  end
+  local content = f:read("*a")
+  f:close()
+  return error_logparser.parse(content)
 end
 
 --- run a command
@@ -35,15 +43,19 @@ local function compile(file)
       local command_template = command_metadata.command
       local command = prepare_command(file, command_template)
       log:debug("command " .. command)
-      if false then
-        -- we reuse this file from make4ht's mkutils.lua
-        local f = io.popen(command, "r")
-        local output = f:read("*all")
-        -- rc will contain return codes of the executed command
-        local rc =  {f:close()}
-        -- the status code is on the third position 
-        -- https://stackoverflow.com/a/14031974/2467963
-        local status = rc[3]
+      -- we reuse this file from make4ht's mkutils.lua
+      local f = io.popen(command, "r")
+      local output = f:read("*all")
+      -- rc will contain return codes of the executed command
+      local rc =  {f:close()}
+      -- the status code is on the third position 
+      -- https://stackoverflow.com/a/14031974/2467963
+      local status = rc[3]
+      if status ~= command_metadata.status then
+        log:error("Command returned wrong status number: " .. (status or ""))
+      end
+      if command_metadata.check_log then
+        local errors = test_log_file(file.basename .. ".log")
       end
     end
   end
