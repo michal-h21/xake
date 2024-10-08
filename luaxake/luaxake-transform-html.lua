ec9dd6aef55faa789ab8d9bed342ff7913bb4aea
Michal Hoftich <michal.h21@gmail.com>
2024-10-08 17:19:53 +0200

    save transformed html

--- luaxake/luaxake-transform-html.lua
+++ luaxake/luaxake-transform-html.lua
@@ -19,16 +19,22 @@ local function find_html_file(file)
 end
 
 
+local html_cache = {}
+
 --- load DOM from a HTML file
 ---@param filename string
 ---@return DOM_Object|nil dom
 ---@return string? error_message
 local function load_html(filename)
-  local f = io.open(filename, "r")
-  if not f then return nil, "Cannot open HTML file: " .. (filename or "") end
-  local content = f:read("*a")
-  f:close()
-  return domobject.html_parse(content)
+  -- cache DOM objects
+  if not html_cache[filename] then 
+    local f = io.open(filename, "r")
+    if not f then return nil, "Cannot open HTML file: " .. (filename or "") end
+    local content = f:read("*a")
+    f:close()
+    html_cache[filename] = domobject.html_parse(content)
+  end
+  return html_cache[filename]
 end
 
 --- detect if the HTML file is xourse
@@ -75,6 +81,19 @@ local function remove_empty_paragraphs(dom)
   end
 end
 
+--- Save DOM to file
+---@param dom DOM_Object
+---@param filename string
+local function save_html(dom, filename)
+  local f = io.open(filename, "w")
+  if not f then
+    return nil, "Cannot save updated HTML: " .. (filename or "")
+  end
+  f:write(dom:serialize())
+  f:close()
+  return true
+end
+
 --- Post-process HTML files
 ---@param file metadata 
 ---@return boolean status
@@ -83,13 +102,12 @@ local function process(file)
   -- we must find metadata for the HTML file, because `file` is metadata of the TeX file
   local html_file, msg = find_html_file(file)
   if not html_file then return false, msg end
-  local dom, msg = load_html(html_file.absolute_path)
+  local html_name = html_file.absolute_path
+  local dom, msg = load_html(html_name)
   if not dom then return false, msg end
   remove_empty_paragraphs(dom)
   local is_xourse = is_xourse(dom, html_file)
-
-
-  return true
+  return save_html(dom, html_name)
 end
 
 M.process = process
